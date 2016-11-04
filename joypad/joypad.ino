4273a81e7d52de768127c65aced3238b1898de24
enrico santoemma <enrico.santoemma@gmail.com>
2016-11-04 18:10:49 +0100

    speed/direction to motor

--- joypad/joypad.ino
+++ joypad/joypad.ino
@@ -1,9 +1,23 @@
+#include <Dynamixel_Serial.h>
+
+#define SERVO_ID_1 0x01
+
+#define SERVO_ControlPin 0x02
+#define SERVO_Baudrate 57600
+
+#define CW_LIMIT_1 1024
+#define CCW_LIMIT_1 3072
+
 String pc_data = "";
 boolean pc_data_complete = false;
 
 void setup() {
   Serial.begin(115200);
+  Dynamixel.begin(SERVO_Baudrate, SERVO_ControlPin);
   delay(1000);
+
+  Dynamixel.setMode(SERVO_ID_1, SERVO, CW_LIMIT_1, CCW_LIMIT_1);
+
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
 }
@@ -18,24 +32,60 @@ void loop() {
     Serial.println("{\"x\": " + String(x) + ", \"y\": " + String(y) + "}");
   }
 
-//  if (pc_data_complete) {
-//    Serial.println(pc_data);
-//    // clear the string:
-//    pc_data = "";
-//    pc_data_complete = false;
-//  }
+  // this handles a simple protocol 'key:value\n'
+  if (pc_data_complete) {
+    int sep = pc_data.indexOf(':');
+    if (sep == -1) {
+      Serial.println("{\"error\": \"no key terminator\"}");
+    } else {
+      String key = pc_data.substring(0, sep);
+      String value = pc_data.substring(sep+1);
+      Serial.println("{\"key\": \"" + key + "\", \"value\": \"" + value + "\" }");
+      execute(key, value);
+    }
+    // clear the string:
+    pc_data = "";
+    pc_data_complete = false;
+  }
+
+  check_serial();
 }
 
-void serialEvent() {
+unsigned int speed_1 = 0;
+unsigned int limit_1 = CW_LIMIT_1;
+void execute(String key, String value) {
+  if (key == "speed_A") {
+    speed_1 = abs(value.toInt());
+    Serial.println("{\"debug\": \"" + String(speed_1) + "\"}");
+  } else if (key == "direction_A") {
+    if (value == "CCW") {
+      limit_1 = CCW_LIMIT_1;
+      Serial.println("{\"debug\": \"ccw\"}");
+    } else {
+      limit_1 = CW_LIMIT_1;
+      Serial.println("{\"debug\": \"cw\"}");
+    }
+  }
+  Dynamixel.servo(SERVO_ID_1, limit_1, speed_1);
+}
+
+// TODO check excess size of pc_data
+void check_serial() {
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
     // add it to the inputString:
-    pc_data += inChar;
+    if (inChar != '\n') {
+      pc_data += inChar;
+    }
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
       pc_data_complete = true;
+      break;
     }
   }
 }
+
+
+
--- xymap.py
+++ xymap.py
@@ -1,15 +1,17 @@
 import json
 import serial
 from inclusion import *
+import time
 
 SPOT = 60
+DIAGONAL_SPOT = 100
 CENTER = 512
 
 maps = [
-  [(0,0), (0,1024), (CENTER-SPOT,CENTER+SPOT), (CENTER-SPOT,CENTER-SPOT)],
-  [(0,1024), (1024,1024), (CENTER+SPOT,CENTER+SPOT), (CENTER-SPOT,CENTER+SPOT)],
-  [(1024,1024), (1024,0), (CENTER+SPOT,CENTER-SPOT), (CENTER+SPOT,CENTER+SPOT)],
-  [(1024,0), (0,0), (CENTER-SPOT,CENTER-SPOT), (CENTER+SPOT,CENTER-SPOT)]
+  [(0,0+DIAGONAL_SPOT), (0,1024-DIAGONAL_SPOT), (CENTER-SPOT,CENTER+SPOT), (CENTER-SPOT,CENTER-SPOT)],
+  [(0+DIAGONAL_SPOT,1024), (1024-DIAGONAL_SPOT,1024), (CENTER+SPOT,CENTER+SPOT), (CENTER-SPOT,CENTER+SPOT)],
+  [(1024,1024-DIAGONAL_SPOT), (1024,0+DIAGONAL_SPOT), (CENTER+SPOT,CENTER-SPOT), (CENTER+SPOT,CENTER+SPOT)],
+  [(1024-DIAGONAL_SPOT,0), (0+DIAGONAL_SPOT,0), (CENTER-SPOT,CENTER-SPOT), (CENTER+SPOT,CENTER-SPOT)]
 ]
 lines = {
   0: [(CENTER,CENTER), (0,CENTER)],
@@ -19,32 +21,62 @@ lines = {
 }
 
 ser = serial.Serial('/dev/ttyACM0', 115200)
-count = 0
+time.sleep(2)
+obj = {'minimo': 1000000000, 'massimo': 0}
 
-while True:
-  s = ser.readline()
-  print s
-  try:
-    payload = json.loads(s)
-    P0 = (payload['x'], payload['y'])
+def main():
+  count = 0
+  t0 = 0
 
-    zone = 4
-    for i in range(len(maps)):
-      if in_polygon(P0, maps[i]):
-        zone = i
-        break
+  while True:
+    s = ser.readline()
+    try:
+      payload = json.loads(s)
 
-    if zone == 4:
-      print 'SPOT'
-    else:
-      speed = point_to_line(P0, lines[zone][0], lines[zone][1])
-      direction = is_left(P0, lines[zone][0], lines[zone][1]) < 0
-      if zone >= 2:
-        direction = not direction
+      # discard too fast joystick positions
+      if payload.has_key('x') and time.time() - t0 < 0.1:
+        continue
+      t0 = time.time()
 
-      print ['A', 'B', 'C', 'D'][zone], speed, {True: 'forward', False: 'backward'}[direction]
+      if payload.has_key('x') and payload.has_key('y'):
+        analyze_position(payload)
+      else:
+        # None
+        print payload
 
-  except ValueError:
-    count += 1
-    if count % 10 == 0:
-      print '10 garbage received'
+    except ValueError:
+      count += 1
+      if count % 10 == 0:
+        # None
+        print '10 garbage received'
+
+
+def analyze_position(payload):
+  P0 = (payload['x'], payload['y'])
+
+  zone = 4
+  for i in range(len(maps)):
+    if in_polygon(P0, maps[i]):
+      zone = i
+      break
+
+  if zone == 4:
+    # None
+    print P0, 'SPOT'
+  else:
+    speed = point_to_line(P0, lines[zone][0], lines[zone][1])
+    direction = is_left(P0, lines[zone][0], lines[zone][1]) < 0
+    if zone >= 2:
+      direction = not direction
+
+    obj['minimo'] = min(obj['minimo'], int(round(speed/8)))
+    obj['massimo'] = max(obj['massimo'], int(round(speed/8)))
+    # print obj
+
+    ser.write('speed_A:%(speed)s\n' % {'speed': int(round(speed))+1})  # TODO better map from speed to Dynamixel speed
+    ser.write('direction_A:%(direction)s\n' % {'direction': {False: 'CCW', True: 'CW'}[direction]})
+
+    print P0, ['A', 'B', 'C', 'D'][zone], speed, {True: 'forward', False: 'backward'}[direction]
+
+if __name__ == "__main__":
+  main()
